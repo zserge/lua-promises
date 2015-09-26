b34a603ca9fe57b0e760dd41c67a12058804562c
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2015-09-26 12:09:11 +0200

    test exit status depends on number of failed tests

--- deferred_test.lua
+++ deferred_test.lua
@@ -1,4 +1,17 @@
-require('gambiarra')()
+local tests_passed = 0
+local tests_failed = 0
+require('gambiarra')(function(e, test, msg)
+	if e == 'pass' then
+		print("[32m✔[0m "..test..': '..msg)
+		tests_passed = tests_passed + 1
+	elseif e == 'fail' then
+		print("[31m✘[0m "..test..': '..msg)
+		tests_failed = tests_failed + 1
+	elseif e == 'except' then
+		print("[31m✘[0m "..test..': '..msg)
+		tests_failed = tests_failed + 1
+	end
+end)
 
 local deferred = require('deferred')
 
@@ -293,3 +306,4 @@ test('Sync of rejecting promises', function()
 	ok(eq(f2.called[1][1], {'foo', 'bar', 'baz'}), 'sync promise rejected with correct args')
 end)
 
+os.exit(tests_failed == 0)
